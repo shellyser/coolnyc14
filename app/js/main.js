8b07bb0eb55d5f65d86afb3307e0a1f3e094593b
Shelly Seroussi <shelly@thinkecoinc.com>
2015-01-02 19:44:32 -0500

    Height adjusts to win height and bg photos are responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
+/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
+(function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
+
+( function( $ ) {
 
 // snippet from http://justinhileman.info/article/a-jquery-widont-snippet/
 // for preventing widowed words.
@@ -14,7 +17,7 @@ $(document).ready(function(){
 	var panel = 3000,
 	transition = 500,
 
-	beforeStreetUpper = transition,
+	beforeStreetUpper = transition + panel/3,
 	afterStreetUpper = beforeStreetUpper + panel,
 	beforeStreetLower = afterStreetUpper + transition,
 	afterStreetLower = beforeStreetLower + panel*2,
@@ -38,219 +41,107 @@ $(document).ready(function(){
 	text1of2in = 400,
 	text1of2out = 1000,
 	text2of2in = 1400,
-	text2of2out = 2000,	
+	text2of2out = 2000
 
 	streetUpperText1In = beforeStreetUpper + text1of2in;
 
 
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$slideTall = $('.homeSlideTall');
+	$slideTall2 = $('.homeSlideTall2');
+	$body = $('body');
+	// windowHeight = $(window).height();
 
-	windowHeight = $(window).height();
+	// if (windowHeight < 800){
+	// 	$('html').addClass('shorty');
+	// }
+	// else{
+	// 	$('html').removeClass('shorty');
+	// }
 
-	if (windowHeight < 800){
-		$('html').addClass('shorty');
-	}
-	else{
-		$('html').removeClass('shorty');
-	}
-
-	$('#intro').height(windowHeight);
+	// $('#intro').height(windowHeight);
 	// $('section').height(windowHeight);
 
+	// function adjustWindow(){
 
-    //nav
+		var scroll = skrollr.init({
+			// scale: 2,
+			// forceHeight: true,
+			render: function(data) {
+		        //Debugging - Log the current scroll position.
+				console.log(data.curTop); 
+			     	if(data.curTop >= 1528){
+			    	    	$('.count').prop('Counter',0).animate({
+			    	        Counter: 2032
+			    	    }, {
+			    	        duration: 2000,
+			    	        easing: 'swing',
+			    	        step: function (now) {
+			    	            $(this).text(Math.ceil(now));
+			    	        }
+			       	 });
+			       	 $('.street-text p').css('visibility', 'visible');		    	     
+			    } else {
+			    	$('.street-text p').css('visibility', 'hidden');
+			    	console.log('Im under 1528');
+			    }
+			 },        
+		    constants: {
+		        bstreetupper: beforeStreetUpper,
+		        astreetupper: afterStreetUpper,
+		        bstreetlower: beforeStreetLower,
+		        astreetlower: afterStreetLower,
+		        bbrick: beforeBrick,
+		        abrick: afterBrick,
+		        btaxi: beforeTaxi,
+		        ataxi: afterTaxi,
+		        bskyline: beforeSkyline,
+		        askyline: afterSkyline,
+		        bfence: beforeFence,
+		        afence: afterFence,
+		        bbridge: beforeBridge,
+		        abridge: afterBridge,
+		        bnycave: beforeNycave,
+		        anycave: afterNycave,
+		        bballoons: beforeBalloons,
+		        aballoons: afterBalloons,
+		        boutro: beforeOutro,
+		        aoutro: afterOutro
+		       }
+		});
 
-    var scrollTime = 1500;
+		
+		// get window size
+		winW = $window.width();
+		winH = $window.height();
 
-	$("#to-intro, #back-to-top").click(function() {
-	    $('body, html').animate({
-	        scrollTop: 0 + 'px'
-	    }, scrollTime);
-	});
+		slideH = winH;
 
-	$("#to-street-upper").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeStreetUpper + 'px'
-	    }, scrollTime);
-	});
+		if(slideH <= 450) {
+			slideH = 450;
+		} 
 
-	$("#to-street-lower").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeStreetLower + 'px'
-	    }, scrollTime);
-	});
+		scroll.refresh();
 
-	$("#to-Brick").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeBrick + 'px'
-	    }, scrollTime);
-	});
+		$('#intro-scroll').delay(1000).fadeIn(2000);
+		$('#intro-year').fadeIn(500);
+		$('#intro-text').fadeIn(500);
+		$('#intro-scroll').each(function() {
+		    var elem = $(this);
+		    setInterval(function() {
+		        if (elem.css('visibility') == 'hidden') {
+		            elem.css('visibility', 'visible');
+		        } else {
+		            elem.css('visibility', 'hidden');
+		        }    
+		    }, 1000);
+		});
+		
+	// }
+// 	adjustWindow();
 
-		$("#to-people").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforePeople + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-participants").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeParticipants + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-map").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeMap + 'px'
-	    }, scrollTime);
-	});
-
-	$("#to-boxes").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeBoxes + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-installer").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeInstaller + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-apartment").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeApartment + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-clock").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeClock + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-heatwave").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeHeatwave + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-stats").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeStats + 'px'
-	    }, scrollTime);
-	});
-
-	$("#to-letter").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeLetter + 1 + 'px'
-	    }, scrollTime);
-	});
-
-	$("#to-phone").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforePhone + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-balloons").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeBalloons + 'px'
-	    }, scrollTime);
-	});
-
-		$("#to-outro").click(function() {
-	    $('body, html').animate({
-	        scrollTop: beforeOutro + 'px'
-	    }, scrollTime);
-	});
-
-	var isPlaying = false;
-
-	$("#play, #intro-play").click(function() {
-		if (!isPlaying){
-			isPlaying = true;
-			var time = 130000*((afterOutro - $(window).scrollTop())/afterOutro);
-		    $('body, html').animate({
-		        scrollTop: afterOutro + 'px'
-		    }, time, 'linear');
-		}
-		else{
-			$('body, html').stop(true);
-			isPlaying = false;
-		}
-	});
-
-	var scroll = skrollr.init({
-		render: function(data) {
-	        //Debugging - Log the current scroll position.
-			console.log(data.curTop); 
-		     	if(data.curTop >= 1528){
-		    	    	$('.count').prop('Counter',0).animate({
-		    	        Counter: 2032
-		    	    }, {
-		    	        duration: 2000,
-		    	        easing: 'swing',
-		    	        step: function (now) {
-		    	            $(this).text(Math.ceil(now));
-		    	        }
-		       	 });
-		       	 $('.street-text p').css('visibility', 'visible');		    	     
-		    } else {
-		    	$('.street-text p').css('visibility', 'hidden');
-		    	console.log('Im under 1528');
-		    }
-		 },        
-		// scale: 2,
-		// forceHeight: true,
-	    constants: {
-	        bstreetupper: beforeStreetUpper,
-	        astreetupper: afterStreetUpper,
-	        bstreetlower: beforeStreetLower,
-	        astreetlower: afterStreetLower,
-	        bbrick: beforeBrick,
-	        abrick: afterBrick,
-	        btaxi: beforeTaxi,
-	        ataxi: afterTaxi,
-	        bskyline: beforeSkyline,
-	        askyline: afterSkyline,
-	        bfence: beforeFence,
-	        afence: afterFence,
-	        bbridge: beforeBridge,
-	        abridge: afterBridge,
-	        bnycave: beforeNycave,
-	        anycave: afterNycave,
-	        bballoons: beforeBalloons,
-	        aballoons: afterBalloons,
-	        boutro: beforeOutro,
-	        aoutro: afterOutro
-	    }
-	});
-
-	$('#intro-scroll').delay(1000).fadeIn(2000);
-	$('#intro-year').fadeIn(500);
-	$('#intro-text').fadeIn(500);
-	$('#intro-scroll').each(function() {
-	    var elem = $(this);
-	    setInterval(function() {
-	        if (elem.css('visibility') == 'hidden') {
-	            elem.css('visibility', 'visible');
-	        } else {
-	            elem.css('visibility', 'hidden');
-	        }    
-	    }, 1000);
-	});
-
-
-});
-
-$(window).resize(function(){
-	windowHeight = $(window).height();
-
-	if (windowHeight < 800){
-		$('html').addClass('shorty');
-	}
-	else{
-		$('html').removeClass('shorty');
-	}
-
-	$('#intro').height(windowHeight);
-})
+// $(window).resize(function() {   
+//     adjustWindow();
+// });
+} )( jQuery );
